9dc9f2819281d55019e96c0770d4d23825cc5cb9
Willow Ahrens <willow.ahrens@mit.edu>
2022-07-18 19:16:12 -0400

    cool

--- src/TensorDepot.jl
+++ src/TensorDepot.jl
@@ -1,5 +1,70 @@
 module TensorDepot
 
-# Write your package code here.
+using Random
+using StatsBase
+using LinearAlgebra
+
+using MatrixDepot
+using MatrixDepot: include_generator, FunctionName, Group, publish_user_generators
+
+"""
+random sparse Stochastic Kronecker tensor
+========================
+stockronrand([rng], [T], D, p, [rand]) = (I, V, dims)
+
+generate a random tensor according to the probability distribution
+
+kron(D...) ./ sum(kron(D...))
+
+*Input options:*
++ [rng]: a random number generator
++ [T]: an element type
++ D: an iterator over probability Arrays
++ p: the number of nonzero values to sample, as an integer or fraction of the total size.
++ [rand]: a random function to generate values
+
+*Outputs:*
++ I: unsorted output coordinate vectors, with duplicates
++ V: The output values, with duplicates
++ dims: The size of the output tensor
+
+*Examples*
+
+The output of this function may be passed to sparse, as:
+
+```
+sparse(mdopen("stockronrand", Iterators.repeated([0.9 0.1; 0.9 0.1], 4), 200).A...)
+```
+"""
+stockronrand(D, m) = stockronrand(Float64, D, m, rand)
+stockronrand(D, m, rand) = stockronrand(Float64, D, m, rand)
+stockronrand(T::Type, D, m) = stockronrand(Random.default_rng(), T, D, m, rand)
+stockronrand(T::Type, D, m, rand) = stockronrand(Random.default_rng(), T, D, m, rand)
+stockronrand(rng::AbstractRNG, D, m) = stockronrand(rng, Float64, D, m, rand)
+stockronrand(rng::AbstractRNG, D, m, rand) = stockronrand(rng, Float64, D, m, rand)
+stockronrand(rng::AbstractRNG, T::Type, D, m) = stockronrand(rng, T, D, m, rand)
+stockronrand(rng::AbstractRNG, T::Type, D, m::AbstractFloat, rand) = 
+    stockronrand(rng, T, D, ceil(Int, mapreduce(length, *, D) * m), rand)
+function stockronrand(rng::AbstractRNG, T::Type, D, m::Integer, rand::Rand) where {Rand}
+    dims = mapreduce(size, .*, D)
+    N = length(dims)
+    D = map(d -> (d ./ sum(d)), D)
+    I = ntuple(_->Int[], N)
+    V = rand(rng, T, dims...)
+    for _ = 1:m
+        i = ntuple(n->1, N)
+        for d in D
+            i = (i .- 1) .* size(D) .+ Tuple(sample(CartesianIndices(size(d)), Weights(reshape(d, :))))
+        end
+        push!.(I, i)
+    end
+    return (A = (I, V, dims), )
+end
+
+function __init__()
+    include_generator(FunctionName, "stockronrand", stockronrand)
+    include_generator(Group, :random, stockronrand)
+    publish_user_generators()
+end
 
 end
