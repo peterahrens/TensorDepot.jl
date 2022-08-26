fa69461db51b5e88c5fbb9ae9e957329148ca4a7
Willow Ahrens <willow.ahrens@mit.edu>
2022-08-26 10:49:38 -0400

    update

--- src/OtherDownload.jl
+++ src/OtherDownload.jl
@@ -189,7 +189,7 @@ grayscale values. This returns the test split from the digits extension of emnis
 """
 function emnist_digits_test()
     dir = joinpath(download_cache, "emnist")
-    EMNIST(:test, dir=dir, Tx=UInt8).digits.features
+    EMNIST(:test, dir=dir, Tx=UInt8).Digits.features
 end
 
 """
@@ -201,7 +201,7 @@ grayscale values. This returns the train split from the digits extentsion of mni
 """
 function emnist_digits_train()
     dir = joinpath(download_cache, "emnist")
-    EMNIST(:train, dir=dir, Tx=UInt8).digits.features
+    EMNIST(:train, dir=dir, Tx=UInt8).Digits.features
 end
 
 """
@@ -213,7 +213,7 @@ grayscale values. This returns the test split from the letters extension of emni
 """
 function emnist_letters_test()
     dir = joinpath(download_cache, "emnist")
-    EMNIST(:test, dir=dir, Tx=UInt8).letters.features
+    EMNIST(:test, dir=dir, Tx=UInt8).Letters.features
 end
 
 """
@@ -225,7 +225,7 @@ grayscale values. This returns the train split from the letters extentsion of mn
 """
 function emnist_letters_train()
     dir = joinpath(download_cache, "emnist")
-    EMNIST(:train, dir=dir, Tx=UInt8).letters.features
+    EMNIST(:train, dir=dir, Tx=UInt8).Letters.features
 end
 
 """
@@ -237,7 +237,7 @@ grayscale values. This returns the test split from the complete emnist.
 """
 function emnist_test()
     dir = joinpath(download_cache, "emnist")
-    EMNIST(:test, dir=dir, Tx=UInt8).byclass.features
+    EMNIST(:test, dir=dir, Tx=UInt8).ByClass.features
 end
 
 """
@@ -249,7 +249,7 @@ grayscale values. This returns the train split from complete emnist.
 """
 function emnist_train()
     dir = joinpath(download_cache, "emnist")
-    EMNIST(:train, dir=dir, Tx=UInt8).byclass.features
+    EMNIST(:train, dir=dir, Tx=UInt8).ByClass.features
 end
 
 """
